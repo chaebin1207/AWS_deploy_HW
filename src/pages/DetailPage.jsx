70ec02675e696fa7e4c2115b7280adb70a8e31bd
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-07-27 16:31:13 +0900

    [Settings] 리액트 프로젝트 생성

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import Button from "../components/Button";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const DetailPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [detail, setDetail] = useState([]);
+  const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+  const getDetail = (id) => {
+    axios
+      .get(`${baseURL}/entries/${id}/`)
+      .then((response) => {
+        console.log(response);
+        setDetail(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteComment = (id) => {
+    axios
+      .delete(`http://127.0.0.1:8000/entries/${id}/`)
+      .then(() => {
+        alert("삭제가 완료되었습니다.");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getDetail(id);
+  }, [id]);
+  return (
+    <Wrapper>
+      <Button txt="게시글 작성하기" onBtnClick={() => navigate("/write")} />
+      <DetailWrapper>
+        <Author>{detail.author}</Author>
+        <Time>{detail.timestamp}</Time>
+        <Comment>{detail.comment}</Comment>
+        <BtnWrapper>
+          <Button
+            txt="수정"
+            fontSize="1.875rem"
+            onBtnClick={() => navigate(`/edit/${detail.id}`)}
+          />
+          <Button
+            txt="삭제"
+            fontSize="1.875rem"
+            onBtnClick={() => deleteComment(id)}
+          />
+        </BtnWrapper>
+      </DetailWrapper>
+    </Wrapper>
+  );
+};
+
+export default DetailPage;
+
+const Wrapper = styled.div`
+  margin-top: 1.25rem;
+  color: var(--text-black);
+`;
+
+const DetailWrapper = styled.div`
+  /* width: calc(100% - 12.5rem); */
+  height: fit-content;
+  background-color: white;
+  border-radius: 1.25rem;
+  padding: 6.25rem;
+  box-shadow: 0 0 0.625rem rgba(0, 0, 0, 0.15);
+  margin-top: 3.125rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Author = styled.div`
+  font-size: 2.5rem;
+  font-weight: 700;
+`;
+
+const Time = styled.div`
+  color: var(--text-grey);
+  font-weight: 600;
+  font-size: 1.25rem;
+`;
+
+const Comment = styled.div`
+  font-size: 1.875rem;
+  font-weight: 700;
+  margin: 3.125rem 0;
+`;
+
+const BtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.25rem;
+`;
